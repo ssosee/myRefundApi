3327fb23979ad8fa81ffcb284df20f4fecb7e5f2
howisitgoing <howisitgoing@kakao.com>
2022-08-15 00:24:20 +0900

    Initial commit

---
+++ src/main/java/seaung/myrefundapi/MyRefundApiApplication.java
@@ -0,0 +1,13 @@
+package seaung.myrefundapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRefundApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRefundApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/seaung/myrefundapi/MyRefundApiApplicationTests.java
@@ -0,0 +1,13 @@
+package seaung.myrefundapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRefundApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
